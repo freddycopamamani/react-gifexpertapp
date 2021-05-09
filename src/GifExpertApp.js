c2bf54c73b54ac17ab08d2bc17ce19cab05fa8c5
Freddy Copa Mamani <copamamanifreddy@gmail.com>
2021-05-09 10:43:34 -0400

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid'
+
+export const GifExpertApp = () => {
+
+
+    const [ categories, setCategories] = useState(['One Punch']);
+
+    // const añadirFruta = () => {
+    //     setFrutas( frut => [...frut, 'Mandarina']);
+    //     //setFrutas( [ 'Palta', ...frutas] )
+    // }
+
+    return (
+        <div>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+            
+            <ol>
+                {
+                    categories.map(category => (
+                        <GifGrid 
+                            key={category}
+                            category={category} 
+                        />
+                    ))
+                }
+            </ol>
+        </div>
+    )
+}
